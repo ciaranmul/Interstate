49a5dd8abfe9ab5febb4a85487bddaf4222a2468
Ciaran Mulholland <ciaranmul@googlemail.com>
2026-04-01 21:24:08 +0100

    Group items by date

--- Interstate/Views/ProjectDetailsView.swift
+++ Interstate/Views/ProjectDetailsView.swift
@@ -11,29 +11,31 @@ import SwiftUI
 struct ProjectDetailsView: View {
     @Bindable var project: Project
     @State private var selectedItem: Item? = nil
+    @State private var selectedListItem: Item? = nil
     @State private var showingAlert: Bool = false
+
+    var items: [Item] {
+        project.items ?? []
+    }
+
+    var groupedItems: [Date: [Item]] {
+        Dictionary(grouping: items) { item in
+            Calendar.current.startOfDay(for: item.timestamp)
+        }
+    }
+
+    var dates: [Date] {
+        Array(groupedItems.keys).sorted(by: {
+            $0.compare($1) == .orderedAscending
+        })
+    }
 
     var body: some View {
         Group {
-            if let items = project.items, !items.isEmpty {
-                List {
-                    ForEach(items.sorted(by: {
-                        $0.timestamp.compare($1.timestamp) == .orderedAscending
-                    })) { item in
-                        let date = Date.FormatStyle(date: .omitted, time: .shortened).format(item.timestamp)
-                        let text = item.entry.isEmpty ? "[No description]" : item.entry
-
-                        Button {
-                            selectedItem = item
-                        } label: {
-                            Text("\(date): \(text)")
-                        }
-                        .buttonStyle(.plain)
-                    }
-                    .onDelete(perform: deleteItems)
-                }
+            if items.isEmpty {
+                emptyStateView
             } else {
-                ContentUnavailableView("You have no entries, yet.", systemImage:"text.document", description: Text("Use the '+' to add a new entry. Tip: you can press 'Return' on your keyboard to add a new entry."))
+                list
             }
         }
         .toolbar {
@@ -46,8 +48,10 @@ struct ProjectDetailsView: View {
                 Button(action: renameProject) {
                     Label("Rename project", systemImage: "pencil")
                 }
+                .keyboardShortcut(.init("r"))
             }
             #endif
+
             ToolbarItem {
                 Button(action: addItem) {
                     Label("Add Item", systemImage: "plus")
@@ -70,6 +74,49 @@ struct ProjectDetailsView: View {
         }
     }
 
+    @ViewBuilder
+    private var emptyStateView: some View {
+        ContentUnavailableView("You have no entries, yet.",
+                               systemImage:"text.document",
+                               description: Text("Use the '+' to add a new entry. Tip: you can press 'Return' on your keyboard to add a new entry."))
+    }
+
+    @ViewBuilder
+    private var list: some View {
+        List(selection: $selectedListItem) {
+            ForEach(dates, id: \.self) { date in
+                Section("\(date.formatted(date: .abbreviated, time: .omitted))") {
+                    ForEach(items(for: date), id: \.self) { item in
+                        listItem(for: item)
+                            .tag(item)
+                    }
+                    .onDelete { indexSet in
+                        deleteItems(offsets: indexSet, date: date)
+                    }
+                }
+            }
+        }
+    }
+
+    @ViewBuilder
+    private func listItem(for item: Item) -> some View {
+        let date = Date.FormatStyle(date: .omitted, time: .shortened).format(item.timestamp)
+        let text = item.entry.isEmpty ? "[No description]" : item.entry
+
+        Button {
+            selectedItem = item
+        } label: {
+            Text("\(date): \(text)")
+        }
+        .buttonStyle(.plain)
+    }
+
+    private func items(for date: Date) -> [Item] {
+        (groupedItems[date] ?? []).sorted(by: {
+            $0.timestamp.compare($1.timestamp) == .orderedAscending
+        })
+    }
+
     private func addItem() {
         withAnimation {
             let newItem = Item()
@@ -78,10 +125,11 @@ struct ProjectDetailsView: View {
         }
     }
 
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                project.items?.remove(at: index)
+    private func deleteItems(offsets: IndexSet, date: Date) {
+        let ids = offsets.map { items(for: date)[$0].id }
+        for id in ids {
+            withAnimation {
+                project.items?.removeAll { $0.id == id }
             }
         }
     }
@@ -89,4 +137,8 @@ struct ProjectDetailsView: View {
     private func renameProject() {
         showingAlert = true
     }
+}
+
+#Preview {
+    ProjectDetailsView(project: .init())
 }
